cae2bec097eba455078a92ae4cd808e98e91ec9d
Chris Girard <chrisegirard@gmail.com>
2023-02-17 11:04:25 -0500

    fixed typo in index.js for models

--- models/index.js
+++ models/index.js
@@ -13,7 +13,7 @@ Category.hasMany(Product, {
   foreignKey: 'category_id'
 })
 // Products belongToMany Tags (through ProductTag)
-Product.belongToMany(Tag, {
+Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
     unique: false
@@ -21,7 +21,7 @@ Product.belongToMany(Tag, {
   as: 'product_tags'
 })
 // Tags belongToMany Products (through ProductTag)
-Tag.belongToMany(Product, {
+Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
     unique: false
